4b00f44efaa674b4e8e741ac07a94eeba8d15547
zhaojing <zhaojing@jiandanla.com>
2018-01-23 19:00:24 +0800

    add something

--- app/src/main/java/blog/com/blogstudy/opengl/custom/DurianGLRender.java
+++ app/src/main/java/blog/com/blogstudy/opengl/custom/DurianGLRender.java
@@ -18,24 +18,24 @@ public class DurianGLRender implements GLSurfaceView.Renderer {
     private Triangle triangle;
     private Square square;
 
-    private Triangle ctriangle;
-    private Square csquare;
+    int mWidth, mHeight;
 
-    public DurianGLRender(Context context){
-        mContext=context;
+    public DurianGLRender(Context context) {
+        mContext = context;
 
-        triangle=new Triangle();
-        square=new Square();
+        triangle = new Triangle();
+        square = new Square();
 
     }
+
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 
-        gl.glClearColor(0.0f,0.0f,0.0f,1.0f);
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         gl.glClearDepthf(1.0f);
         gl.glEnable(GL10.GL_DEPTH_TEST);
         gl.glDepthFunc(GL10.GL_LEQUAL);
-        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT,GL10.GL_NICEST);
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
         gl.glShadeModel(GL10.GL_SMOOTH);
         gl.glDisable(GL10.GL_DITHER);
 
@@ -43,17 +43,21 @@ public class DurianGLRender implements GLSurfaceView.Renderer {
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-        if(height==0){
-            height=1;
+        if (height == 0) {
+            height = 1;
         }
-        float aspect=(float)width/height;
+        mWidth = width;
+        mHeight = height;
+        float aspect = (float) width / height;
 
-        gl.glViewport(0,0,width,height);
+        gl.glViewport(0, 0, width, height);
 
+        //投影变换
         gl.glMatrixMode(GL10.GL_PROJECTION);
         gl.glLoadIdentity();
-        GLU.gluPerspective(gl,45,aspect,0.1f,100.0f);
+        GLU.gluPerspective(gl, 45, aspect, 0.1f, 100.0f);
 
+        //模型变换
         gl.glMatrixMode(GL10.GL_MODELVIEW);
         gl.glLoadIdentity();
 
@@ -62,15 +66,19 @@ public class DurianGLRender implements GLSurfaceView.Renderer {
     @Override
     public void onDrawFrame(GL10 gl) {
 
-        gl.glClear(GL10.GL_COLOR_BUFFER_BIT|GL10.GL_DEPTH_BUFFER_BIT);
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 
         gl.glLoadIdentity();
-        gl.glTranslatef(-3.0f,0.0f,-6.0f);
-        gl.glRotatef(30, 1, 0, 0);
-        GLU.gluLookAt(gl,0,0,500,0,0,200,upX,upY,upZ);
+        gl.glTranslatef(-3.0f, 0.0f, -6.0f);
+        /**
+         * 绕（1,0,0）向量旋转90度：gl.glRotatef(90, 1, 0, 0);
+         * 沿x轴方向移动1个单位：gl.glTranslatef(1, 0, 0);
+         * x，y，z方向放缩0.1倍：gl.glScalef(0.1f, 0.1f, 0.1f);
+         */
+//        GLU.gluLookAt(gl, 500, 500, 0, mWidth / 2, mHeight / 2, 0, 0, 0, 0);
         triangle.draw(gl);
 
-        gl.glTranslatef(3.0f,0.0f,1.0f);
+        gl.glTranslatef(3.0f, 0.0f, 1.0f);
         square.draw(gl);
     }
 
